acda4bca6fb410fae30c7643c3b70be593d74d20
Neo767 <matoane.ss@gmail.com>
2023-03-18 11:31:07 +0000

    send it

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
         return 0
 
     roman_to_int_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-'M': 1000}
+	'M': 1000}
     result = 0
     prev_val = 0
 
